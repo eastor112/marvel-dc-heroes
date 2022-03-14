ab2816d3636d066f93a423e76c1536700094fc98
Emerson Asto <emerar.mct@gmail.com>
2022-03-14 13:31:05 -0500

    configured authContext and authReducer

--- src/HeroesApp.tsx
+++ src/HeroesApp.tsx
@@ -1,9 +1,25 @@
+import { useReducer } from 'react';
+import { AuthContext } from './auth/authContext';
+import { authReducer } from './auth/authReducer';
 import AppRouter from './routers/AppRouter';
+
+const initState = () => {
+  return localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user') || '') : { isAuthenticated: false }
+}
+
+
 const HeroesApp = () => {
 
-  return (
+  const [user, dispatch] = useReducer(authReducer, {}, initState);
 
-    <AppRouter />
+  return (
+    < AuthContext.Provider value={{
+      user,
+      dispatch
+    }}>
+      <AppRouter />
+    </ AuthContext.Provider>
+
 
   )
 }
--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/authContext';
 
 export const Navbar = () => {
+
+  const { user } = useContext(AuthContext);
+  console.log(user);
 
   const navigate = useNavigate()
 
@@ -49,7 +54,7 @@ export const Navbar = () => {
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
           <span className='nav-item nav-link text-info'>
-            Emerson
+            {user.name}
           </span>
           <button
             className="nav-item nav-link btn btn-primary"
